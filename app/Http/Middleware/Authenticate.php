7798bc25aad9ac978d82710af3629cdb02043b94
EC2 Default User <ec2-user@ip-172-31-47-172.us-east-2.compute.internal>
2018-03-07 14:19:42 +0000

    user login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -38,9 +38,10 @@ class Authenticate
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('auth/login');
-            }
+                return redirect()->guest(route('login.get'));            }
         }
+        
+         
 
         return $next($request);
     }
